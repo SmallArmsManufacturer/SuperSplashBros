0b7d7d7e9b1fd253b86841be2d88265917567c83
James Robinson <admin@robinjam.net>
2012-06-20 12:58:31 +0100

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,63 @@
+#include <GLUT/glut.h>
+#include <OpenGL/gl.h>
+
+namespace
+{
+	// The default window size
+	const int WIDTH = 1024, HEIGHT = 768;
+
+	void init()
+	{
+		// Set the background colour to light blue
+		glClearColor(0.5f, 0.6f, 1.0f, 1.0f);
+
+		// Set up the modelview matrix (i.e. position the camera 10 metres along the positive Z axis)
+		glMatrixMode(GL_MODELVIEW);
+		glLoadIdentity();
+		glTranslated(0.0, 0.0, -10.0);
+	}
+
+	void reshape(int width, int height)
+	{
+		// Resize the OpenGL viewport
+		glViewport(0, 0, width, height);
+
+		// Set up the projection matrix with a vertical FOV of 45 degrees with the near clipping plane 1 metre away and the far one 100 metres away
+		glMatrixMode(GL_PROJECTION);
+		glLoadIdentity();
+		gluPerspective(45.0, (double) width / height, 1.0, 100.0);
+	}
+
+	void display()
+	{
+		// Clear the screen
+		glClear(GL_COLOR_BUFFER_BIT);
+		
+		// Draw a white, solid, 1x1x1 meter cube at the origin
+		glColor3d(1.0, 1.0, 1.0);
+		glutSolidCube(1.0);
+
+		// Swap the buffers
+		glFlush();
+		glutSwapBuffers();
+
+		// Tell GLUT to re-run the display func as soon as possible
+		glutPostRedisplay();
+	}
+}
+
+int main(int argc, char *argv[])
+{
+	// Create the main window
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_DEPTH);
+	glutInitWindowSize(WIDTH, HEIGHT);
+	glutCreateWindow("OpenGL demo");
+
+	// Initialise and register callbacks
+	init();
+	reshape(WIDTH, HEIGHT);
+	glutReshapeFunc(reshape);
+	glutDisplayFunc(display);
+	glutMainLoop();
+}
